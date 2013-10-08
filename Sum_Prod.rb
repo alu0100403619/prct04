56eeeecee1c2a2e9a0f631f7643a693c2025bb05
Jorge O. Blanchard Cruz <rothgard@rothgard-VirtualBox.(none)>
2013-10-08 11:18:38 +0000

    2er: read & show implementados

---
+++ Sum_Prod.rb
@@ -0,0 +1,30 @@
+def read(mat, fils, cols)
+  for i in (0...fils)
+    aux = Array.new()
+    for j in (0...cols)
+      print "mat[#{i}][#{j}]: "
+      value = gets
+      #mat[i][j] = value.to_i
+      aux << value.to_i
+    end #for j
+    mat << aux
+  end #for i
+end
+
+def show(mat)
+  for i in (0...mat.length)
+    for j in (0...mat[0].length)
+      print "#{mat[i][j]}  "
+    end #for j
+    puts ""
+  end #for i
+end
+
+fils = 2
+cols = 2
+mat1 = Array.new{Array.new()}
+puts "Leyendo Matriz 1"
+read(mat1, fils, cols)
+mat2 = Array.new{Array.new()}
+puts "Leyendo Matriz 2"
+read(mat2, fils, cols)
