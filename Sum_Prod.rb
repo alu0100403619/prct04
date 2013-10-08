b2c8816f71e92e94e40433788527630bccf4c957
Jorge O. Blanchard Cruz <rothgard@rothgard-VirtualBox.(none)>
2013-10-08 11:21:24 +0000

    4to: sum implementado

--- Sum_Prod.rb
+++ Sum_Prod.rb
@@ -20,6 +20,19 @@ def show(mat)
   end #for i
 end
 
+def sum(mat_a, mat_b)
+  sum = Array.new()
+
+  for i in (0...mat_a.length)
+    sum[i] = Array.new()
+    for j in (0...mat_b[0].length)
+      sum [i][j] = (mat_a[i][j] + mat_b[i][j])
+    end #for j
+  end #for i
+
+  sum
+end
+
 fils = 3
 cols = 3
 mat1 = Array.new{Array.new()}
@@ -28,3 +41,9 @@ read(mat1, fils, cols)
 mat2 = Array.new{Array.new()}
 puts "Leyendo Matriz 2"
 read(mat2, fils, cols)
+
+sum = suma(mat1, mat2)
+puts "Mostrando Resultado Suma"
+show(sum)
+
+
